c42deafb56976e9c4fe60023db887147a89745fb
Frank Kelly <frankintech00@gmail.com>
2023-02-23 11:36:27 +0000

    added image assets

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../assets/images/logo.png';
+
+export const Header = () => {
+	return (
+		<div>
+			<img src={logo} alt='' className='w-10 h-10'></img>
+		</div>
+	);
+};
